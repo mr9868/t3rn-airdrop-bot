0d8cfdba3712d0352f84b620719bf197823b0797
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-09-09 14:28:43 +0700

    Update blast.js

--- blast.js
+++ blast.js
@@ -1,13 +1,13 @@
-require('colors');
-const { Wallet, JsonRpcProvider, ethers, parseUnits } = require('ethers');
-const fs = require('fs');
+require("colors");
+const { Wallet, JsonRpcProvider, ethers, parseUnits } = require("ethers");
+const fs = require("fs");
 
-const readlineSync = require('readline-sync');
-const moment = require('moment');
-const T3RN_ABI = require('./contracts/ABI_blast');
-const { displayHeader } = require('./utils/display');
-const { transactionData, delay } = require('./utils/helper');
-const { getAmount } = require('./utils/api');
+const readlineSync = require("readline-sync");
+const moment = require("moment");
+const T3RN_ABI = require("./contracts/ABI_blast");
+const { displayHeader } = require("./utils/display");
+const { transactionData, delay } = require("./utils/helper");
+const { getAmount } = require("./utils/api");
 
 const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
 const RPC_URL = T3RN_ABI.at(-1).RPC_ARBT;
@@ -17,25 +17,25 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
 
 (async () => {
   displayHeader();
-  console.log('⏳ Please wait...'.yellow);
-  console.log('');
+  console.log("⏳ Please wait...".yellow);
+  console.log("");
 
   const options = readlineSync.question(
-    'Choose the network that you want to use 👇\n1. Blast Sepolia to Base Sepolia\n2. Blast Sepolia to Arbitrum Sepolia\n3. Blast Sepolia to Optimism Sepolia\n4. Exit\n\nEnter 1, 2, 3, or 4: '
+    "Choose the network that you want to use 👇\n1. Blast Sepolia to Base Sepolia\n2. Blast Sepolia to Blast Sepolia\n3. Blast Sepolia to Optimism Sepolia\n4. Exit\n\nEnter 1, 2, 3, or 4: "
   );
 
-  if (options === '4' || !options) {
-    console.log('👋 Exiting the bot. See you next time!'.cyan);
-    console.log('Subscribe: https://t.me/HappyCuanAirdrop.'.green);
+  if (options === "4" || !options) {
+    console.log("👋 Exiting the bot. See you next time!".cyan);
+    console.log("Subscribe: https://t.me/HappyCuanAirdrop.".green);
     process.exit(0);
   }
 
   const numTx = readlineSync.questionInt(
-    '🔄 How many times you want to swap or bridge? '
+    "🔄 How many times you want to swap or bridge? "
   );
 
   if (numTx <= 0) {
-    console.log('❌ Number of transactions must be greater than 0!'.red);
+    console.log("❌ Number of transactions must be greater than 0!".red);
     process.exit(1);
   }
 
@@ -46,18 +46,18 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
     while (totalSuccess < numTx) {
       try {
         const balance = await provider.getBalance(wallet.address);
-        const balanceInEth = ethers.formatUnits(balance, 'ether');
+        const balanceInEth = ethers.formatUnits(balance, "ether");
 
         console.log(
           `⚙️ [ ${moment().format(
-            'HH:mm:ss'
+            "HH:mm:ss"
           )} ] Doing transactions for address ${wallet.address}...`.yellow
         );
 
         if (balanceInEth < 0.001) {
           console.log(
             `❌ [ ${moment().format(
-              'HH:mm:ss'
+              "HH:mm:ss"
             )} ] Your balance is too low (💰 ${balanceInEth} ETH), please claim faucet first!`
               .red
           );
@@ -76,8 +76,9 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
               continue;
             }
 
-            const request = "0x56591d5962737370000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000062a459f164fbb4acf8be5e2fed615dd85baa40700000000000000000000000000000000000000000000000000238610fdcf980e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002386f26fc10000";
-            const gasPrice = parseUnits('0.1', 'gwei'); // adjustable
+            const request =
+              "0x56591d5962737370000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000062a459f164fbb4acf8be5e2fed615dd85baa40700000000000000000000000000000000000000000000000000238610fdcf980e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002386f26fc10000";
+            const gasPrice = parseUnits("0.1", "gwei"); // adjustable
 
             const transaction = {
               data: request,
@@ -85,25 +86,25 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
               gasLimit: 2000000, // adjustable
               gasPrice,
               from: wallet.address,
-              value: parseUnits('0.0001', 'ether'), // adjustable
+              value: parseUnits("0.01", "ether"), // adjustable
             };
 
             const result = await wallet.sendTransaction(transaction);
             console.log(
               `✅ [ ${moment().format(
-                'HH:mm:ss'
+                "HH:mm:ss"
               )} ] Transaction successful from Blast Sepolia to ${
-                options === '1' ? 'Base' : options === '2' ? 'Arbitrum' : 'OP'
+                options === "1" ? "Base" : options === "2" ? "Arbitrum" : "OP"
               } Sepolia!`.green
             );
             console.log(
               `🔗 [ ${moment().format(
-                'HH:mm:ss'
+                "HH:mm:ss"
               )} ] Transaction hash: https://sepolia.blastscan.io/tx/${
                 result.hash
               }`.green
             );
-            console.log('');
+            console.log("");
 
             totalSuccess++;
             counter--;
@@ -114,7 +115,7 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
           } catch (error) {
             console.log(
               `❌ [ ${moment().format(
-                'HH:mm:ss'
+                "HH:mm:ss"
               )} ] Error during transaction: ${error}`.red
             );
           }
@@ -122,22 +123,22 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
       } catch (error) {
         console.log(
           `❌ [ ${moment().format(
-            'HH:mm:ss'
+            "HH:mm:ss"
           )} ] Error in processing transactions: ${error}`.red
         );
       }
     }
   }
 
-  console.log('');
+  console.log("");
   console.log(
     `🎉 [ ${moment().format(
-      'HH:mm:ss'
+      "HH:mm:ss"
     )} ] All ${numTx} transactions are complete!`.green
   );
   console.log(
     `📢 [ ${moment().format(
-      'HH:mm:ss'
+      "HH:mm:ss"
     )} ] Subscribe: https://t.me/HappyCuanAirdrop`.green
   );
 })();
