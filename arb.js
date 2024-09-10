a3e8e381def5c9eff44c0f2805494b647ae2e654
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-09-10 23:14:08 +0700

    Update arb.js

--- arb.js
+++ arb.js
@@ -92,7 +92,7 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
               const min = 0.01;
               const max = 0.05;
               const randomValue = Math.random() * (max - min) + min;
-              const rValue = randomValue.toFixed(2).toString();
+              const rValue = randomValue.toFixed(2);
               // return {
                 // value: parseUnits(randomValue.toFixed(2), "ether"),
                 // amountInEth: randomValue.toFixed(2)
@@ -107,8 +107,7 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
               gasLimit: 2000000, // adjustable
               gasPrice,
               from: wallet.address,
-              value: parseUnits(rValue, "ether"),
-              // value: parseUnits("0.01", "ether"),
+              value: parseUnits("${rValue}", "ether"),
             };
 
             const result = await wallet.sendTransaction(transaction);
