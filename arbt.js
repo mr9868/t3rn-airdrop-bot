8f2f85d3c0c4cf62108e327dbf927979abed1f66
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-13 19:02:47 +0700

    Update arbt.js

--- arbt.js
+++ arbt.js
@@ -31,12 +31,11 @@ const CONTRACT_ADDRESS = T3RN_ABI.at(-1).CA_ARBT;
     console.log('Subscribe: https://t.me/HappyCuanAirdrop.'.green);
     process.exit(0);
   }
-  const jumlah = readlineSync.questionInt(
+ const jumlah = readlineSync.questionFloat(
     "🔄 How much amount you want to bridge ? => "
   );
   const awal = 10000000000000000;
-  const ambilJumlah = jumlah.toFixed(0);
-  const jumlahTx = ambilJumlah * 100000;
+  const jumlahTx = jumlah * 100000;
   const patAwal = Number(String(awal).replace("1000",jumlahTx));
   const desimal = String(jumlahTx / 100000);
   const saldoHex = patAwal.toString(16);
