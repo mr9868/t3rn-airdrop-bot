c3c4ae3a1a361175cdb0575476a3a9559b78fcb4
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-13 16:17:42 +0700

    Update aku.js

--- aku.js
+++ aku.js
@@ -3,6 +3,7 @@
   const max = 1111;
   const randomValue = Math.random() * (max - min) + min;
   const baru = randomValue.toFixed(0);
+  const desimal = baru / 10000
 
 // convert number to a string, then extract the first digit
 var final = Number(String(nolsatu).replace("1000",baru));
@@ -10,5 +11,6 @@ var final = Number(String(nolsatu).replace("1000",baru));
 // convert the first digit back to an integer
 // var one_as_number = Number(one);
 // var tot  = one.replace(one,aku);
-console.log(nolsatu.toString(16));
-console.log(final.toString(16));
+console.log(nolsatu));
+console.log(final);
+console.log(desimal);
