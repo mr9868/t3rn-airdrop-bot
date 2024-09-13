f5a6c7b44c809d6b266b8da48b249145538c6bf9
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-13 21:55:44 +0700

    Update aku2.js

--- aku2.js
+++ aku2.js
@@ -8,7 +8,27 @@ const readlineSync = require('readline-sync');
   const desimal = String(jumlahTx / 100000);
   const saldoHex = patAwal.toString(16);
 console.log("000" + saldoHex);
+console.log("000" + saldoHex);
 console.log(desimal);
 console.log(patAwal);
 console.log(jumlahTx);
 console.log(jumlah);
+
+const nolsatu = 51000000000000000;
+  const min = 1000;
+  const max = 1111;
+  const randomValue = Math.random() * (max - min) + min;
+  const acak = randomValue.toFixed(0);
+  const desimal = String(acak / 100000);
+
+// convert number to a string, then extract the first digit
+var final = Number(String(nolsatu).replace("1000",acak));
+const saldo = nolsatu.toString(16);
+console.log(desimal);
+console.log(nolsatu);
+console.log(final);
+console.log(saldo);
+let num = 42;
+
+let paddedNum = String(saldo).padStart(16, '0'); // '00042'
+console.log(paddedNum);
