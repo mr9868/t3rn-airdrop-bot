b0eedbdf0c87c7476153133d68756f5de52fc8be
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-13 16:05:12 +0700

    Create aku.js

---
+++ aku.js
@@ -0,0 +1,14 @@
+  const nolsatu = 10000000000000000;
+  const min = 1000;
+  const max = 1111;
+  const randomValue = Math.random() * (max - min) + min;
+  const baru = randomValue.toFixed(0);
+
+// convert number to a string, then extract the first digit
+var final = Number(String(nolsatu).replace("1000",baru));
+
+// convert the first digit back to an integer
+// var one_as_number = Number(one);
+// var tot  = one.replace(one,aku);
+console.log(nolsatu.toString(16));
+console.log(final.toString(16));
