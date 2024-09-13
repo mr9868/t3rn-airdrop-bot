3f30c8d4345466d01b739f938ec0d410a9a434e7
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-13 18:58:34 +0700

    Update aku2.js

--- aku2.js
+++ aku2.js
@@ -1,1 +1,15 @@
 
+ const jumlah = readlineSync.questionInt(
+    "🔄 How much amount you want to bridge ? => "
+  );
+  const awal = 10000000000000000;
+  const ambilJumlah = jumlah.toFixed(0);
+  const jumlahTx = ambilJumlah * 100000;
+  const patAwal = Number(String(awal).replace("1000",jumlahTx));
+  const desimal = String(jumlahTx / 100000);
+  const saldoHex = patAwal.toString(16);
+console.log(saldoHex);
+console.log(desimal);
+console.log(patAwal);
+console.log(jumlahTx);
+console.log(ambilJumlah);
