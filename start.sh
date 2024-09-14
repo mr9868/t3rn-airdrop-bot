d9839c5edc1402dfe6593871618835e6705f5815
M.Nur <32473994+mnur9868@users.noreply.github.com>
2024-09-14 16:24:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,13 +9,13 @@ echo "WELCOME TO T3RN AIRDROP BOT !\n
          
         ";
 read -p "Please select :" answer
-if [[ $answer = 1 ]] ; then
+if [[ $answer == 1 ]] ; then
   node arbt.js
-elif [[ $answer = 2 ]] ; then
+elif [[ $answer == 2 ]] ; then
   node bssp.js
-elif [[ $answer = 3 ]] ; then
+elif [[ $answer == 3 ]] ; then
   node blss.js
-elif [[ $answer = 4 ]] ; then
+elif [[ $answer == 4 ]] ; then
   node opsp.js
 else
   echo -e "Nothing to do !\n"
